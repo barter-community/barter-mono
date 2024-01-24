60892d9725ab72efe630af70edc1f43cea444d7a
0x0scion <flash@sector.finance>
2024-01-23 21:28:08 -0500

    rm dep

--- barter-execution-rs/src/model/order_event.rs
+++ barter-execution-rs/src/model/order_event.rs
@@ -6,7 +6,7 @@ use thiserror::Error;
 use crate::fill::{Decision, MarketMeta};
 
 /// All errors generated in the barter::portfolio module.
-#[derive(Error, Debug)]
+#[derive(Copy, Clone, Error, Debug)]
 pub enum OrderEventError {
     #[error("Failed to build struct due to missing attributes: {0}")]
     BuilderIncomplete(&'static str),
